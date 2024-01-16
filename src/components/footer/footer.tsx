5939b05dd908728a13b4834601cef17086651ef9
meggsan <meggie.sanchez@gmail.com>
2024-01-16 21:39:18 +0100

    fix: fix footer icons style and dark mode toggle in chrome

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -11,15 +11,15 @@ const LINKEDIN_LINK_URL = 'https://www.linkedin.com/in/meggie-sanchez';
 export const Footer = () => {
   return (
     <footer>
-      <a className={styles.footerIcon} href={LINKEDIN_LINK_URL} target='_blank' rel='noopener noreferrer'>
+      <a className={styles.footerIconContainer} href={LINKEDIN_LINK_URL} target='_blank' rel='noopener noreferrer'>
         <LinkedinIcon /> &nbsp;&nbsp;LinkedIn
       </a>
       <span className={styles.pipeSeparator}>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-      <a className={styles.footerIcon} href={GITHUB_LINK_URL} target='_blank' rel='noopener noreferrer'>
+      <a className={styles.footerIconContainer} href={GITHUB_LINK_URL} target='_blank' rel='noopener noreferrer'>
         <GithubIcon /> &nbsp;&nbsp;GitHub
       </a>
       <span className={styles.pipeSeparator}>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-      <a className={styles.footerIcon} href={`mailto:${PERSONAL_MAIL}`}>
+      <a className={styles.footerIconContainer} href={`mailto:${PERSONAL_MAIL}`}>
         <MailIcon /> &nbsp;&nbsp;Mail
       </a>
     </footer>
