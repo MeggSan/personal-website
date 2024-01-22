a92bbdb1ffb1f7389d8e0a220035f110d692c401
meggsan <meggie.sanchez@gmail.com>
2024-01-22 16:15:34 +0100

    feature: add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
@@ -17,5 +19,49 @@ export default defineConfig({
       '@interfaces': path.resolve(__dirname, './src/interfaces'),
     },
   },
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'MS Website',
+        name: 'Meggie Sanchez Website',
+        description: 'Application to show information about Meggie Sanchez, her portfolio and her personal projects',
+        icons: [
+          {
+            src: './logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any',
+          },
+          {
+            src: './logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any',
+          },
+          {
+            src: './maskableLogo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
+          },
+          {
+            src: './maskableLogo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+        ],
+        start_url: '/',
+        display: 'standalone',
+        display_override: ['standalone', 'fullscreen'],
+        theme_color: '#673b72',
+        background_color: '#242424',
+        prefer_related_applications: false,
+        scope: '/',
+      },
+    }),
+  ],
 });
