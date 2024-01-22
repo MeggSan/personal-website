bb4b32f4a82db03c43859566d40e67d9f3e40960
meggsan <meggie.sanchez@gmail.com>
2024-01-22 14:32:14 +0100

    feature: add click outside of menu event handler

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Navbar } from '@components/navbar/navbar';
@@ -13,13 +13,27 @@ import styles from './header.module.css';
 export const Header = () => {
   const { darkMode } = useContext(DarkModeContext);
   const [showMenu, setShowMenu] = useState(false);
+  const isMenuDisplayedRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutsideMenu, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutsideMenu);
+    };
+  }, []);
 
   const onClickMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  const handleClickOutsideMenu = ({ target }: MouseEvent): void => {
+    if (!isMenuDisplayedRef.current?.contains(target as Node)) {
+      setShowMenu(false);
+    }
+  };
+
   return (
-    <header>
+    <header ref={isMenuDisplayedRef}>
       <div className={styles.header}>
         <Link to={`/`}>
           <picture className={styles.pictureLogo}>
