133ef2027c52a01edbcef249373eb155b219794b
meggsan <meggie.sanchez@gmail.com>
2024-02-05 22:30:15 +0100

    fix: pusheen img update

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 
 import { TypingEffectText } from '@components/typingEffectText/typingEffectText';
-import { PusheenIcon } from '@assets/icons/pusheenIcon';
+import PusheenImg from '@assets/images/pusheen-img.webp';
 
 import styles from './home.module.css';
 
@@ -13,7 +13,7 @@ export const Home = () => {
       <h3 className={`${styles.titleText} ${styles.mediumSizeText}`}>{t('home.hello1')}</h3>
       <div className={styles.nameContainer}>
         <h1 className={`${styles.titleText} ${styles.bigSizeText}`}>{t('home.name')}</h1>
-        <PusheenIcon />
+        <img src={PusheenImg} alt='Pusheen Image' width={121} height={82} />
       </div>
       <span className={`${styles.titleText} ${styles.bigSizeText}`}>
         {t('home.hello2')}&nbsp;
